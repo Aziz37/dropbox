e3089fd1473bf77a49b2a327c6ac3e256d0e3b40
Aziz37 <abdul_aziz37@hotmail.com>
2018-11-15 00:34:39 +0600

    initial commit

---
+++ database/seeds/FoldersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FoldersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('folders')->insert([
+            'admin_id'      =>  0,
+            'root_id'		=>	0,
+        	'parent_id'     =>  0,
+        	'name'          =>  'Test Folder'
+        ]);
+    }
+}
